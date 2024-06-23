aa397ca0ab427418ac4e18d0626bb39ff0101f2e
chi1180 <CWChi1@outlook.jp>
2024-06-24 06:45:55 +0900

    ads added

--- projects/script.js
+++ projects/script.js
@@ -2,3 +2,11 @@
 /** 新しいタブでリンクを開くようにする */
 const link_s = document.querySelector("main").querySelectorAll("a");
 link_s.forEach( link => { link.target = "_blank" } );
+
+
+setTimeout(() => {
+    const ads = document.querySelector(".left-ads");
+const article = document.querySelector(".article");
+ads.style.height = article.clientHeight + "px";
+console.log(ads.clientHeight);
+}, 250);
