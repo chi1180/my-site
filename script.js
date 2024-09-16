c1f210a0a535f0c1286e25672572c3202bf4109c
Chi_1 <143685104+chi1180@users.noreply.github.com>
2024-09-16 15:52:37 +0900

    latest version

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+const ARTICLES = [
+    {
+        "title": "サイトを新しくしました",
+        "description": "いろいろと考えた末に、サイトを新しく作り直しました。",
+        "content": "### 新しいサイト\n騒がしかった時期をきっかけにして、Qiitaにも、このサイトにもアップデートをしない時期が続きました。\n\nそこで、このままではいけないと、やっと思い立ってすぐ、サイトの作り直しにかかりました。\n\n不要なコンテンツを無くし、**何のために作ったのか**を大事にして作っていきたいと思います。\n### お願い\n\n[contacts](#contacts)では私にメッセージを送っていただくことが可能です。\n\n励みになりますので、もしよろしければ、このサイトの感想や改善案、そのほかについてメッセージを送信していただくと非常にうれしく思います。\n\n有難うございました。",
+        "created_at": "2024/09/16"
+    }
+];
+
+
+
+
+
+
+
+
+
+
+function articlePreview(article_data)
+{
+    let text = "# " + article_data["title"] + "\n" + article_data["description"] + "\n" + article_data["content"] + "\n\ncreated_at : " + article_data["created_at"];
+    const html = marked.parse(text);
+    blog_preview.innerHTML = html;
+}
+
+const blog_preview = document.querySelector(".blog-preview");
+const blog_container = document.querySelector(".blog-container");
+
+for (let i = ARTICLES.length - 1; i > -1; i --) {
+    const [blog_card, blog_title, blog_description] = Array(3).fill().map(() => document.createElement("div"));
+    blog_card.classList.add("blog-card");
+    blog_title.classList.add("blog-title");
+    blog_description.classList.add("blog-description");
+    blog_title.innerText = ARTICLES[i].title;
+    blog_description.innerText = ARTICLES[i].description;
+    blog_card.addEventListener("click", () => {
+        articlePreview(ARTICLES[i]);
+    });
+
+    blog_card.appendChild(blog_title);
+    blog_card.appendChild(blog_description);
+
+    blog_container.appendChild(blog_card);
+
+    if (i === ARTICLES.length - 1) articlePreview(ARTICLES[i]);
+}
+
+const lastEditDate = document.getElementById("last-edit-date");
+lastEditDate.innerText = ARTICLES[ARTICLES.length - 1]["created_at"];
+
+
 const nav = document.querySelector("nav");
 
 nav.querySelectorAll("a").forEach((a, index) => {
@@ -76,5 +127,4 @@ document.querySelectorAll("a").forEach(a => {
 nav.querySelectorAll("a").forEach(a => {
     a.target = "_self";
 });
-
 
